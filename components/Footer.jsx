f5e044395be4700f8c9303f0116e9a9c71b62def
Keval Vora <41047220+kvora125@users.noreply.github.com>
2024-03-28 12:48:17 +0530

    cosmetic changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <footer className="bg-light p-3 text-center" data-testid="footer">
     <p data-testid="footer-text">
-      Turininx Assignment Project Made with <a href="https://nextjs.org/">NextJS</a> and  <a href="https://auth0.com">Auth0</a>
+      Turinix Assignment Project Made with <a href="https://nextjs.org/">NextJS</a> and  <a href="https://auth0.com">Auth0</a>
     </p>
   </footer>
 );
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,18 +4,19 @@ import Logo from './Logo';
 
 const Hero = () => (
   <div className="my-0 mx-auto max-w-[768px] my-5 text-center" data-testid="hero">
-    <div className='flex'>
-      <Logo testId="hero-logo" /> 
+    <div className='flex flex-row col-gap-10'>
+      <Logo /> 
+      <img className='logo mx-2'  />
     </div>
     <h1 className="mb-4" data-testid="hero-title">
-      Keval's Auth0 based SignUp/SignIn user authentication Project.
+      Keval's Auth0 based SignUp/SignIn User Authentication Project.
     </h1>
 
     <p className="lead" data-testid="hero-lead">
       This is a application that demonstrates an authentication flow for a NextJS Web App. 
       It demonstrates Google, Facebook and Github IDP login and normal email-password login.
-      It secures all passwrodless logins via MFA and all password based logins in risky situations via MFA too. 
-      Since the authentication is cookie based sessions are persistent.
+      It secures all passwordless logins via MFA and all password based logins in risky situations via MFA too. 
+      Since the authentication is cookie based sessions are persistent. Link to my resume in navbar is login protected.
     </p>
   </div>
 );
