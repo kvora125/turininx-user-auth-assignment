85c39f18479ba4816c1ec89a608ff9de66ec51e9
Keval Vora <41047220+kvora125@users.noreply.github.com>
2024-03-28 12:54:57 +0530

    test env changes

--- tests/environment.js
+++ tests/environment.js
@@ -1,5 +1,5 @@
-process.env.AUTH0_SECRET = 'session_secret';
-process.env.AUTH0_ISSUER_BASE_URL = 'https://acme.auth0.local';
-process.env.AUTH0_BASE_URL = 'http://www.acme.com/';
-process.env.AUTH0_CLIENT_ID = 'client_id';
-process.env.AUTH0_CLIENT_SECRET = 'client_secret';
+process.env.AUTH0_SECRET = 'replace-with-your-own-secret-generated-with-openssl';
+process.env.AUTH0_ISSUER_BASE_URL = 'https://cyberform.au.auth0.com';
+process.env.AUTH0_BASE_URL = 'http://localhost:3000';
+process.env.AUTH0_CLIENT_ID = 'laMmpxZj8mZ9kgZFi6xjlgLlO7rSoDZL';
+process.env.AUTH0_CLIENT_SECRET = '5t0A8deQ7vObDQLBt03DGDlLN8ngFnPbtFmLewhf3EaHZbWgWrKoOMh4u6ilzCqB';
